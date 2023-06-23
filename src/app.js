cc53c0369f69f14d83e9b7b171ad7bc253909f76
Caio Lucas <clpmb2000@gmail.com>
2023-06-23 15:53:08 -0300

    Post dos tweets feito

--- src/app.js
+++ src/app.js
@@ -6,49 +6,47 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-let usuarios = [];
-let tweets = [];
-let Avatar = "";
+const usuarios = [];
+const tweets = [];
 
 app.post("/sign-up", (req, res) => {
-    const {username, avatar} = req.body;
-    const newUser = {username, avatar};
-    Avatar = avatar;
-    usuarios.push(newUser);
-    res.send("OK");
+    const user = req.body;
+    usuarios.push(user);
+    res.status(200).send(usuarios);
 });
 
 app.post("/tweets", (req, res) => {
-
-    const user = usuarios.find(user => user.username === username);
-
-    if(user){
-        const {username, tweet} = req.body;
-        const newTweet = {username, tweet};
-        tweets.push(newTweet);
-        res.send("OK");
-    }else{
-        res.send("UNAUTHORIZED");
+    const { username, tweet } = req.body;
+    const user = usuarios.find((user) => user.username === username);
+  
+    if (!user) {
+        res.status(401).send("UNAUTHORIZED");
     }
-    
-})
+
+    const newTweet = { 
+        username: username,
+        avatar: user.avatar,
+        tweet: tweet
+    };
+      tweets.push(newTweet);
+      res.status(200).send(tweets);});
 
 app.get("/tweets", (req, res) => {
-    if(tweets.length===0){
+    if (tweets.length === 0) {
         res.send([]);
         return;
     }
-    app.get("/tweets", (req, res) => {
-        const {username, tweet} = req.body;
-        const novoTweet = [{
-            username: username,
-            avatar: Avatar,
-            tweet: tweet
-        }]
-        });
-    
-        res.send(novoTweet);
+
+    let novosTweets = [];
+
+    if(tweets.length > 10){
+        novosTweets = tweets.slice(tweets.length - 10);
+    }else{
+        novosTweets = tweets.slice();
+    }
+    console.log(novosTweets);
+    res.send(novosTweets);
 });
-    
+
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
